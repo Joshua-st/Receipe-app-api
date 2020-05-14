f8acb916e6808947fdddefba6d050cf9bda656d8
joshua-st <joshua@os.university>
2020-05-14 21:26:18 +0200

    testing repo

---
+++ testing.py
@@ -0,0 +1,18 @@
+
+
+# testing the gitlab repo
+
+# simple python scrip
+number1 = 1
+number2 = 4
+
+
+# a function that adds two given numbers
+def adding_two_numbers(var1, var2):
+    answer = var1 + var2
+
+    return answer
+
+
+# testing the function
+print(adding_two_numbers(number1, number2))
