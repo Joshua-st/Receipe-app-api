93a8986ad8c601229162f80eacb0fb0aaf1fba29
joshua-st <joshua@os.university>
2020-05-15 07:44:45 +0200

    testing a simple function

--- testing.py
+++ testing.py
@@ -3,16 +3,18 @@
 # testing the gitlab repo
 
 # simple python scrip
-number1 = 1
+number1 = 4
 number2 = 4
 
 
 # a function that adds two given numbers
-def adding_two_numbers(var1, var2):
-    answer = var1 + var2
+def dividing_a_sum_of_two(var1, var2):
+    sum = var1 + var2
+    answer = sum/2
+
 
     return answer
 
 
 # testing the function
-print(adding_two_numbers(number1, number2))
+print(dividing_a_sum_of_two(4,8))
